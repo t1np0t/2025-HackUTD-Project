5bf02121d29e0760d1fbbd4a0668bd2af8bd0d0a
Darrion Nguyen <111015952+t1np0t@users.noreply.github.com>
2025-11-09 02:20:11 -0600

    updated ui

--- app/compare.tsx
+++ app/compare.tsx
@@ -1,3 +1,6 @@
 export default function comparePage() {
+    <div className=""
     
+    >
+    </div>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,47 +2,50 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import React from "react";
-import vehicleData from "../scripts/vehicleData.json"
+import vehicleData from "../scripts/vehicleData.json";
 
 export default function Home() {
   const [selectedVehicles, setSelectedVehicle] = useState<number[]>([]);
-  const selectItem = (index:number) => {
-    setSelectedVehicle(prev => prev.includes(index) ? prev.filter(i => i !== index) : [...prev, index])
-  }
+  const selectItem = (index: number) => {
+    setSelectedVehicle((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
+    );
+  };
   const router = useRouter();
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-white">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white">
-        <div className="w-full min-h-screen flex items-center justify-center">
-          <button
-            className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 bg-blue-600 text-white px-5 py-2 rounded shadow-lg"
-            onClick={() => router.push("/compare.tsx")}
-          >
-            Compare
-          </button>
-          <div className="grid grid-cols-5 gap-5 text-center text-black sm:items-start sm:text-left w-full min-w-7xl">
-            {vehicleData.map((info, index) => (
-              <div
-                className={`border-4 rounded-xl cursor-pointer p-2 ${
-                  selectedVehicles.includes(index)
-                    ? "border-green-500"
-                    : "border-gray-900"
-                }`}
-                key={index}
-                onClick={() => selectItem(index)}
-              >
-                <b>{info.name}</b>
-                <p>{info.year}</p>
-                <p>{info.seatCount} seats</p>
-                <div className="grid grid-cols-2">
-                  <p>{info.combinedMpg} mpg </p>
-                </div>
-                <img src={info.vehicleImage} />
-              </div>
-            ))}
-          </div>
+    <div className="flex min-h-screen justify-end bg-zinc-50 font-sans dark:bg-white relative">
+      <button
+          className="fixed top-0 w-full left-1/2 -translate-x-1/2 text-2xl cursor-pointer z-50 bg-red-600 text-white px-5 py-3 rounded shadow-lg"
+        onClick={() => router.push("/compare")}
+      >
+        <b className="inline-block hover:scale-115 transition-all duration-250">Compare</b>
+      </button>
+
+      <main className="flex flex-row min-h-screen w-full bg-white pt-16">
+        <div className="flex-1 text-black p-4 bg-gray-400 w-1/4 self-start h-full">
+          test
+        </div>
+
+        <div className="grid grid-cols-3 gap-5 text-center text-black w-3/4 p-4">
+          {vehicleData.map((info, index) => (
+            <div
+              className={`border-4 rounded-xl cursor-pointer p-2 ${
+                selectedVehicles.includes(index)
+                  ? "border-green-500"
+                  : "border-gray-900"
+              }`}
+              key={index}
+              onClick={() => selectItem(index)}
+            >
+              <b> {info.name} - {info.year} </b>
+              <p> {info.seatCount} seats, {info.combinedMpg} mpg </p>
+              <img
+                src={info.vehicleImage}
+                alt={`${info.name} ${info.year}`}
+              />
+            </div>
+          ))}
         </div>
       </main>
     </div>
